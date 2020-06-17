c626c5583a497cfcb3bf4b6e498f3fadb588608e
zjm <1142376484@qq.com>
2020-06-17 17:22:44 +0800

    test

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,12 +2,15 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.example.demo2.dao")
 public class Demo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
+		// 测试
 	}
 
 }
